3c34a0482fa4965ddc8c67cf6bb49fff9ad7eebe
Zack Wallace <zwallace0790@gmail.com>
2019-03-21 04:29:15 -0700

    Added to .gitignore and heroku deployment stuff

--- solvrr/solvrr.py
+++ solvrr/solvrr.py
@@ -18,4 +18,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.10.42', port=5000)
+    app.run(host='0.0.0.0', port=5000)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from solvrr.py import app
+
+if __name__ == '__main__':
+    app.run()
